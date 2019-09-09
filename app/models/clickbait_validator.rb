09c17eef9c5ecca293c9ba24ee814e69683c0ae6
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-09 18:18:23 +0000

    Automatically backed up by Learn

--- app/models/clickbait_validator.rb
+++ app/models/clickbait_validator.rb
@@ -1,7 +1,8 @@
 class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
-      record.errors[attribute] << ("Not a clickbait title")
-    end
+    # unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+    #   record.errors[attribute] << ("Not a clickbait title")
+    # end
+    if !value.include?("Won't Believe") || !value.include?("Secret") || !value.include?("Top")
   end
 end
