2fc8a6e7eabd3df041ebd1c97eac200a05d8a2f5
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-09 18:13:56 +0000

    Automatically backed up by Learn

--- app/models/clickbait_validator.rb
+++ app/models/clickbait_validator.rb
@@ -1,7 +1,7 @@
 class ClickbaitValidator < ActiveModel::Validator
   def validate(record)
     if !record.title.include? "Won't Believe"
-      
+      record.errors.add(:title, "Must be clickbaity")
     end
   end
 end
