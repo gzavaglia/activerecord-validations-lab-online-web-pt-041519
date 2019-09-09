395dbff94ad8c19bdbb3ced8e6fedd6c10dcf2e6
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-09 17:45:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,11 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
   
   def non_clickbait
     if !title.include?("Won't Believe") || !title.include?("Secret") || !title.include?("Guess") || !title.include?("Top")
+      errors.add(:title, "Title is not clickbait-y enough")
+    end
   end
 end
